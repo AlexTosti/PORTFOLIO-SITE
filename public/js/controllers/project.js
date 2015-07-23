7b0bd83e475dc7126d07fe6686785d8c48bcbdd7
Lucy Match <lucy.matchett@gmail.com>
2015-07-23 13:44:47 -0400

    content update

--- public/js/controllers/project.js
+++ public/js/controllers/project.js
@@ -6,6 +6,11 @@ angular.module('app.controller.project', [])
 .controller('ProjectCtrl', function ($scope, $rootScope) {
 
   console.log('PROJECT CONTROLLER');
-	// $scope.projects = $rootScope.global.projects;
+
+  //show hide quick and dirty drop down
+  $scope.QnD = false;
+
+  ///IMAGE FOR TROPE.. MAKE THIS VAR BASED SO IT PULLS HEADER IMG FROM OBJECT DEPENDING ON WHICH PAGE WE AT
+  $scope.imgURL = 'http://mfadt.parsons.edu/2015/wp-content/uploads/projects/158_img_01.jpg';
 
 });
